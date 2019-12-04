b6a855b6390867a7eea7d0b4acde4feda9b76203
Roger Milroy <rogermilroy@icloud.com>
2019-12-04 17:45:15 +0000

    Fix graphical nn model bugs.

--- Code/grin/src/models/graphical_nn_model.py
+++ Code/grin/src/models/graphical_nn_model.py
@@ -28,9 +28,9 @@ class KalmanGNN(nn.Module):
         self.hy = None
 
         # mlps for edge encoding.
-        self.past_curr_nn = GraphEncoderMLP(2 * self.h_dim, self.h_dim, self.h_dim)
-        self.future_curr_nn = GraphEncoderMLP(2 * self.h_dim, self.h_dim, self.h_dim)
-        self.y_curr_nn = GraphEncoderMLP(2 * self.h_dim, self.h_dim, self.h_dim)
+        self.past_curr_nn = GraphEncoderMLP(2 * self.h_dim + self.x_dim, self.h_dim, self.h_dim)
+        self.future_curr_nn = GraphEncoderMLP(2 * self.h_dim + self.x_dim, self.h_dim, self.h_dim)
+        self.y_curr_nn = GraphEncoderMLP(2 * self.h_dim + self.x_dim, self.h_dim, self.h_dim)
 
         # mlp for node encoding. TODO do I need three layers for this one?
         self.node_nn = GraphEncoderMLP(self.h_dim, self.h_dim, self.h_dim)
@@ -38,7 +38,7 @@ class KalmanGNN(nn.Module):
         # gru for recurrence
         self.gru = nn.GRUCell(input_size=self.h_dim, hidden_size=self.h_dim)
 
-        self.decoder = GraphDecoder(self.h_dim, self.h_dim, self.y_dim)
+        self.decoder = GraphDecoder(self.h_dim, self.h_dim, self.x_dim)
 
     def forward(self, hx, graphical_messages):
         """
@@ -53,24 +53,24 @@ class KalmanGNN(nn.Module):
             hx = hx.t()
 
         # construct the edges to pass through the relevant models
-        hx_past = torch.cat([hx[0], hx[:-1]]).t()
-        hx_future = torch.cat([hx[1:], hx[-1]]).t()
+        hx_past = torch.cat([hx[0].unsqueeze(0), hx[:-1]]).t()
+        hx_future = torch.cat([hx[1:], hx[-1].unsqueeze(0)]).t()
         hx = hx.t()
 
         # pass compute edge encodings.
-        past_curr_edge = self.past_curr_nn(torch.cat([hx_past, hx, past_curr_mess]))
-        fut_curr_edge = self.future_curr_nn(torch.cat([hx_future, hx, fut_curr_mess]))
-        y_curr_edge = self.y_curr_nn(torch.cat([self.hy, hx, y_curr_mess]))
+        past_curr_edge = self.past_curr_nn(torch.cat([hx_past, hx, past_curr_mess]).t()).t()
+        fut_curr_edge = self.future_curr_nn(torch.cat([hx_future, hx, fut_curr_mess]).t()).t()
+        y_curr_edge = self.y_curr_nn(torch.cat([self.hy, hx, y_curr_mess]).t()).t()
 
         # sum edge encodings?
         # pass through node encoder
-        h = self.node_nn(sum([past_curr_edge, fut_curr_edge, y_curr_edge]))
+        h = self.node_nn(sum([past_curr_edge, fut_curr_edge, y_curr_edge]).t()).t()
 
         # pass through gru
-        h = self.gru(h, hx)
+        h = self.gru(h.t(), hx.t()).t()
 
         # return the decoded hx and hx.
-        eps = self.decoder(h)
+        eps = self.decoder(h.unsqueeze(0)).squeeze(0)
 
         return eps, h
 
@@ -87,17 +87,17 @@ class KalmanGNN(nn.Module):
             ys = ys.t()
         num_samples = ys.shape[0]
         # take the difference between past and current ys and current and future ys.
-        y_past = torch.cat([ys[0], ys[:-1]]).t()
-        y_future = torch.cat([ys[1:], ys[-1]]).t()
+        y_past = torch.cat([ys[0].unsqueeze(0), ys[:-1]]).t()
+        y_future = torch.cat([ys[1:], ys[-1].unsqueeze(0)]).t()
         ys = ys.t()
         diff1 = ys - y_past
         diff2 = y_future - ys
 
-        # concatenate and pass through a one dimensional convolution. unsqueeze
-        hy_in = torch.cat([diff1, diff2]).t().unsqueeze(0)
-
-        # save as hy
-        self.hy = self.hy_initialiser(hy_in.unsqueeze(0)).squeeze(0)  # needs squeezing as convs only take 3d inputs
+        # concatenate and  unsqueeze
+        hy_in = torch.cat([diff1, diff2]).unsqueeze(0)
+        # pass through a one dimensional convolution.
+        # save as self.hy
+        self.hy = self.hy_initialiser(hy_in).squeeze(0)  # needs squeezing as convs only take 3d inputs
         hx = torch.randn((self.h_dim, num_samples))
 
         return hx
