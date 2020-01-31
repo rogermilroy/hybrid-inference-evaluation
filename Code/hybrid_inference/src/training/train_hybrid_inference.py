2dbe1711de327860b9d71d6d5f094ee384663176
Roger Milroy <rogermilroy@icloud.com>
2020-01-31 11:17:04 +0000

    Remove shuffle. Add writing to log file. Move time recording into training method.

--- Code/hybrid_inference/src/training/train_hybrid_inference.py
+++ Code/hybrid_inference/src/training/train_hybrid_inference.py
@@ -1,8 +1,8 @@
 from src.models.hybrid_inference_model import HybridInference
 from src.data.synthetic_position_dataloader import get_dataloaders
 import torch
-from torch.nn.functional import mse_loss
 from torch.optim import Adam
+from time import time
 from tqdm import tqdm
 from .evaluate_model import evaluate_model
 
@@ -72,7 +72,7 @@ def train_hybrid_inference(epochs, val, loss, weighted, save_path, log_path="./t
     print("Model on CUDA?", next(model.parameters()).is_cuda)
 
     criterion = loss
-    optimizer = Adam(model.parameters(), lr=0.0001)  # reduce the learning rate to 0.0001 from 0.001
+    optimizer = Adam(model.parameters(), lr=0.0005)  # reduce the learning rate to 0.0001 from 0.001
 
     if data_params:
         # if we have specified specific training parameters use them
@@ -82,7 +82,6 @@ def train_hybrid_inference(epochs, val, loss, weighted, save_path, log_path="./t
         sample_length = data_params["sample_length"]
         starting_point = data_params["starting_point"]
         batch_size = data_params["batch_size"]
-        shuffle = data_params["shuffle"]
         extras = data_params["extras"]
         train_loader, val_loader, test_loader = get_dataloaders(train_samples=train_samples,
                                                                 val_samples=val_samples,
@@ -90,27 +89,31 @@ def train_hybrid_inference(epochs, val, loss, weighted, save_path, log_path="./t
                                                                 sample_length=sample_length,
                                                                 starting_point=starting_point,
                                                                 batch_size=batch_size,
-                                                                shuffle=shuffle,
                                                                 extras=extras)
     else:
         # else use the defaults.
         train_loader, val_loader, test_loader = get_dataloaders()
 
     with open(log_path, 'w+') as log_file:
+        start = time()
         for i in range(epochs):
             # train a simple epoch and record and print the losses.
             epoch_loss, epoch_losses = train_one_epoch(model=model, loader=train_loader,
                                                        optimizer=optimizer, criterion=criterion,
                                                        device=computing_device, weighted=weighted)
             print("Epoch {} avg training loss: {}".format(i+1, epoch_loss/len(train_loader)))
+            log_file.write("Epoch {} avg training loss: {}\n".format(i + 1, epoch_loss / len(
+                train_loader)))
 
             if val:
                 # if we are validating then do that and print the results
                 val_loss, val_av_loss = evaluate_model(model=model, loader=val_loader,
                                                        criterion=criterion,
-                                                       weighted=weighted ,device=computing_device)
+                                                       weighted=weighted, device=computing_device)
                 print("Epoch {} validation loss: {}".format(i + 1, val_loss))
                 print("Epoch {} avg validation loss: {}".format(i + 1, val_av_loss))
+                log_file.write("Epoch {} validation loss: {}\n".format(i + 1, val_loss))
+                log_file.write("Epoch {} avg validation loss: {}\n".format(i + 1, val_av_loss))
             # save the model at this point.
             torch.save(model.state_dict(), save_path)
 
@@ -118,5 +121,9 @@ def train_hybrid_inference(epochs, val, loss, weighted, save_path, log_path="./t
         test_loss, test_av_loss = evaluate_model(model=model, loader=test_loader, criterion=criterion,
                                                  weighted=weighted, device=computing_device,
                                                           vis_example=vis_examples)
+        print("Time taken: {}".format(time() - start))
         print("Total test loss: {}".format(test_loss))
         print("Test average loss: {}".format(test_av_loss))
+        log_file.write("Time taken: {}\n".format(time() - start))
+        log_file.write("Total test loss: {}\n".format(test_loss))
+        log_file.write("Test average loss: {}\n".format(test_av_loss))
