c13eab560630d17f5a9bee2ed28a33a37b9a7c34
Roger Milroy <rogermilroy@icloud.com>
2020-02-03 11:18:16 +0000

    Add simple model comparison method.

--- Code/hybrid_inference/src/training/evaluate_model.py
+++ Code/hybrid_inference/src/training/evaluate_model.py
@@ -1,6 +1,8 @@
 from tqdm import tqdm
 import torch
 from src.models.hybrid_inference_model import HybridInference
+from src.data.synthetic_position_dataloader import get_dataloaders
+from torch.nn.functional import mse_loss
 
 
 def evaluate_model(model, loader, criterion, weighted, device, vis_example=0):
@@ -32,18 +34,54 @@ def evaluate_model(model, loader, criterion, weighted, device, vis_example=0):
     return epoch_loss, epoch_loss / len(loader)
 
 
-if __name__ == '__main__':
-    model = HybridInference(F=torch.tensor([[1., 1., 0., 0.],
-                                            [0., 1., 0., 0.],
-                                            [0., 0., 1., 1.],
-                                            [0., 0., 0., 1.]]),
-                            H=torch.tensor([[1., 0., 0., 0.],
-                                            [0., 0., 1., 0.]]),
-                            Q=torch.tensor([[0.05 ** 2, 0., 0., 0.],
-                                            [0., 0.05 ** 2, 0., 0.],
-                                            [0., 0., 0.05 ** 2, 0.],
-                                            [0., 0., 0., 0.05 ** 2]]),
-                            R=(0.05 ** 2) * torch.eye(2),
+def compare_models(path_to_model1: str, path_to_model2: str):
+    # need datasets
+    train_loader, val_loader, test_loader = get_dataloaders(train_samples=10,
+                                                            val_samples=10,
+                                                            test_samples=200,
+                                                            sample_length=10,
+                                                            starting_point=1,
+                                                            batch_size=4,
+                                                            extras=False)
+    # create models
+    F = torch.tensor([[1., 1., 0., 0.],
+                      [0., 1., 0., 0.],
+                      [0., 0., 1., 1.],
+                      [0., 0., 0., 1.]])
+    H = torch.tensor([[1., 0., 0., 0.],
+                      [0., 0., 1., 0.]])
+    Q = torch.tensor([[0.05 ** 2, 0., 0., 0.],
+                      [0., 0.05 ** 2, 0., 0.],
+                      [0., 0., 0.05 ** 2, 0.],
+                      [0., 0., 0., 0.05 ** 2]])
+    R = (0.05 ** 2) * torch.eye(2)
+    model1 = HybridInference(F=F,
+                            H=H,
+                            Q=Q,
+                            R=R,
                             gamma=1e-4)
-    model.load_state_dict(torch.load("./hybrid_inference_params.pt"))
+    model2 = HybridInference(F=F,
+                            H=H,
+                            Q=Q,
+                            R=R,
+                            gamma=1e-4)
+    # load parameters
+    model1.load_state_dict(torch.load(path_to_model1, map_location=torch.device("cpu")))
+    model2.load_state_dict(torch.load(path_to_model2, map_location=torch.device("cpu")))
+
+    # evaluate and print/return results.
+    loss1, av_loss1 = evaluate_model(model=model1, loader=test_loader, criterion=mse_loss,
+                                     weighted=False, device=torch.device('cpu'))
+    loss2, av_loss2 = evaluate_model(model=model2, loader=test_loader, criterion=mse_loss,
+                                     weighted=False, device=torch.device('cpu'))
+
+    print("Total loss for Model 1: {}".format(loss1))
+    print("Average loss for Model 1: {}".format(av_loss1))
+    print("Total loss for Model 2: {}".format(loss2))
+    print("Total loss for Model 2: {}".format(av_loss2))
+
+
+if __name__ == '__main__':
+    compare_models("../../mse_results/train_len10000_mse_start0_seq10.pt",
+                   "../../weighted_mse_results/weighted_train_len1000_mse_start0_seq10.pt")
 
